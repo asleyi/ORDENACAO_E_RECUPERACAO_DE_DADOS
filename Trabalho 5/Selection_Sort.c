14cc16204e2a9bdfad9a3ba21286daa66a0c4645
Asley <32104658+asleyi@users.noreply.github.com>
2020-02-15 16:33:07 -0300

    Update Selection_Sort.c

--- Trabalho 5/Selection_Sort.c
+++ Trabalho 5/Selection_Sort.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int vetor[7]={9,7,5,2,1,0,3};
+
+    int i;
+    printf("Vetor desordenado: ");
+    for(i=0;i<7;i++){
+        printf("|%d|",vetor[i]);
+    }
+    printf("\n");
+
+    for(i=0;i<sizeof(vetor);i++){
+
+        int min=vetor[i];
+        int j;
+
+        for(j=i;j<7;j++){
+            if(vetor[j]<vetor[i]){
+            min=vetor[j];
+            vetor[j]=vetor[i];
+            vetor[i]=min;
+            }
+        }
+    }
+
+    printf("Vetor ordenado: ");
+    for(i=0;i<7;i++){
+        printf("|%d|",vetor[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
 
